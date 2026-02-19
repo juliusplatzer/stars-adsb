d6a9222af6d739ddb772ebed11b7bacd9c17b373
juliusplatzer <julius.platzer@tum.de>
2026-02-19 16:38:40 +0100

    feat: add index.ts for client

---
+++ src/client/src/index.ts
@@ -0,0 +1,4 @@
+export { fetchAircraftFeed } from "./api.js";
+export { fetchQnhByIcao } from "./api.js";
+export { fetchWxReflectivity } from "./api.js";
+export type { FetchAircraftFeedOptions, FetchQnhOptions, FetchWxReflectivityOptions } from "./api.js";
